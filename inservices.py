68cb37ba48fd484c1fca9e35141a925b5cd43afe
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2018-06-26 17:30:52 +0300

    Enabling flask shell

--- inservices.py
+++ inservices.py
@@ -1,7 +1,10 @@
-from app.routes import app
+from app import app, db
+from app.models import User, UserToken
 
-def main():
-    app.run(host='0.0.0.0', debug=True)
-
-if __name__ == '__main__':
-    main()
+@app.shell_context_processor
+def make_shell_context():
+    return{
+        'db': db,
+        'User': User,
+        'UserToken': UserToken
+    }
