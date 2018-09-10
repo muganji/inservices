d61757820da6103ff735452dc97383213b310074
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2018-09-10 12:44:56 +0300

    random password generator for WebSMAP module

--- app/websmap/handlers.py
+++ app/websmap/handlers.py
@@ -1,3 +1,6 @@
+import string
+import random
+
 from app.models import WebSmapUserToken
 from app import db
 
@@ -12,3 +15,16 @@ def store_token(user_public_id, token):
 def valid_user(user):
     """Checks if user profile is valid and has no conflicts"""
     return (user.valid_username() and user.valid_public_id())
+
+def password_generator(size=6, chars=string.ascii_letters + string.digits):
+    """
+    Returns a string of random characters, useful in generating temporary
+    passwords for automated password resets.
+    
+    size: default=8; override to provide smaller/larger passwords
+    chars: default=A-Za-z0-9; override to provide more/less diversity
+    
+    Credit: Ignacio Vasquez-Abrams
+    Source: http://stackoverflow.com/a/2257449
+    """
+    return ''.join(random.choice(chars) for i in range(size))
