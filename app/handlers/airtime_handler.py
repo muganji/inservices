b28a4ff482120375fa5c078f38fc4de7f95791ab
Julius Muganji <muganji93@gmail.com>
2018-11-22 12:42:46 +0300

    WOT

--- app/handlers/airtime_handler.py
+++ app/handlers/airtime_handler.py
@@ -1,72 +1,147 @@
 # The airtime_handler.py handles the business logic that comprises of crediting and debiting airtime to a valid MSIDN.
-import os
+from intelecom.intelecom import INConnection
 
-class creditAirtime(object):
-    def msidnvalidity(self, msidn):
+from app import app
+
+from app.handlers.profile import account_balance, account_info, profile_status
+
+
+from app.models.user import User
+
+
+class creditAirtime():
+    def msidnvalidity(self, msidn: str, current_user: User) -> bool:
         """
         The msidnValidity method checks whether the supplied msidn exists, returns True if yes and False otherwise.
         argument = a msidn of a client or agent.
         return status either True or False.
         """
-        # TODO Must be implement to be querried from the database file..
-        
-        self.status =False
-        if msidn.exist():
-            self.status = True
+        isValid = False
+        userProfile = profile_status(msidn, current_user)
+        if msidn in iter(userProfile.values()):
+            isValid = True
         else:
-            self.status = False
-            print('This ',msidn ,'to be credited does not exist')
-        return self.status
-         #len(profile.query.filter_by(msidn=msidn).all()) <1 
-      
-            
-    def paying_account(self,msidn):
-        # TODO Must be implement to be querried from the database file..
-        self.status =False
-        if msidn.exist():
-            self.status = True
+            print("The supplied ",msidn," doesn't exist in the database.")
+            isValid = False
+        return isValid
+
+
+    def paying_account(self,msidn: str, current_user: User) -> bool:
+        isValid =False
+        userProfile = profile_status(msidn, current_user)
+        if msidn in iter(userProfile.values()):
+            isValid = True
         else:
-            self.status = False
+            isValid = False
             print('This paying account number',msidn ,'does not exist')
-        #self.status
-        return self.status
-        
-    def check_Crediting_Account_Balance(self,msidn):
-        _accounts =[]
-        for acct, i in _accounts:
-            if msidn ==i:
-                return acct.balance,
-            return True
+        return isValid
 
-        return
-    def check_isaccount_bal_GTE(self,msidn,amount):
-        accounts = {}
-        
 
-        return
-    def check_isaccount_bal_LTE(self,msidn,amount):
-        return
-    def deduct_amount_from_msidn(self,amount):
-        return
-    def credit_msidn(self,msidn,amount):
-        return
-class debitAirtime(object):
-    def msidnvalidity(self, msidn):
-        self.status =False
-        if msidn.exist():
-            self.status = True
+    def check_Crediting_Account_Balance(self, msidn: str, current_user: User) -> str:
+        acc = account_balance(msidn, current_user)
+        balance: str
+        if msidn in iter(acc.values()):
+            balance = acc.get('balance')
         else:
-            self.status = False
-            print('This ',msidn ,'to be credited does not exist')
-        return self.status
+            print("The supplied ",msidn," doesn't have balance.")
+        return balance
+
+    def check_is_account_bal_GTE(self, msidn: str, current_user: User, amount: float) -> bool:
+        isBalanceGTE = False
+        acc = account_balance(msidn, current_user)
+        if msidn in iter(acc.values()):
+            if acc['balance'] >= amount:
+                isBalanceGTE = True
+            else:
+                isBalanceGTE = False
+        return isBalanceGTE
         
-    def msidn_balance_status(self,msidn):
-        pass
-    def deduct_amount(self,amount):
-        pass
-    def debit_isaccount_bal_GTE(self,msidn,amount):
-        return 
-    def debit_isaccount_bal_LTE(self,msidn,amount):
+    def check_is_account_bal_LTE(self,msidn: str, current_user: User,amount: float) -> bool:
+        isBalanceLTE = False
+        acc = account_balance(msidn, current_user)
+        if msidn in iter(acc.values()):
+            if acc['balance'] <= amount:
+                isBalanceLTE = True
+            else:
+                isBalanceLTE = False
+        return isBalanceLTE
+        
+
+    def deduct_amount_from_msidn(self,msidn: str, current_user: User,amount: float) -> str:
+        newBalance: float
+        acc = account_balance(msidn, current_user)
+        if msidn in iter(acc.values()):
+            balance = float(acc.get('balance'))
+            newBalance = balance - amount
+            balToString = acc['balance'] = newBalance
+            return balToString
+        else:
+            print('We only debit a valid msidn')
         return
-    
-    
+
+    def credit_msidn(self,msidn: str, current_user: User,amount: float) -> str:
+        newBalance: float
+        acc = account_balance(msidn, current_user)
+        if msidn in iter(acc.values()):
+            balance = float(acc.get('balance'))
+            newBalance = balance + amount
+            balToString = acc['balance'] = newBalance
+            return balToString
+        else:
+            print('We only debit a valid msidn')
+        return
+
+
+class debitAirtime():
+    def msidnvalidity(self, msidn: str, current_user: User) -> bool:
+        isValid = False
+        userProfile = profile_status(msidn, current_user)
+        if msidn in iter(userProfile.values()):
+            isValid = True
+        else:
+            print("The supplied ",msidn," doesn't exist in the database.")
+            isValid = False
+        return isValid
+        
+    def msidn_balance_status(self,msidn: str, current_user: User) -> float:
+        acc = account_balance(msidn, current_user)
+        balance: str
+        if msidn in iter(acc.values()):
+            balance = acc.get('balance')
+        else:
+            print("The supplied ",msidn," doesn't have balance.")
+        return balance
+
+    def deduct_amount(self,msidn: str, current_user: User,amount: float)-> float:
+        newBalance: float
+        acc = account_balance(msidn, current_user)
+        if msidn in iter(acc.values()):
+            balance = float(acc.get('balance'))
+            newBalance = balance - amount
+            balToString = acc['balance'] = newBalance
+            return balToString
+        else:
+            print('We only debit a valid msidn')
+        return
+
+
+    def debit_is_account_bal_GTE(self,msidn: str, current_user: User,amount: float) -> bool:
+        isBalanceGTE = False
+        acc = account_balance(msidn, current_user)
+        if msidn in iter(acc.values()):
+            if acc['balance'] >= amount:
+                isBalanceGTE = True
+            else:
+                isBalanceGTE = False
+        return isBalanceGTE
+
+
+def debit_is_account_bal_LTE(self,msidn: str, current_user: User,amount: float) -> bool:
+        isBalanceLTE = False
+        acc = account_balance(msidn, current_user)
+        if msidn in iter(acc.values()):
+            if acc['balance'] <= amount:
+                isBalanceLTE = True
+            else:
+                isBalanceLTE = False
+        return isBalanceLTE
--- tests/unit_tests/handler_tests/test_airtime_handler.py
+++ tests/unit_tests/handler_tests/test_airtime_handler.py
@@ -1,92 +1,187 @@
 # Testing the credit/Debit airtime handler.
-import unittest
-from unittest.mock import Mock, patch
-from nose.tools import assert_is_not_none
-from app.handlers.airtime_handler import creditAirtime,debitAirtime
-class testCreditAirtimeMethods(unittest.TestCase):
-    """
-    The testCreditAirtimeMethods is a class that ontains all the proposed tests performed onto the debit and credit modules.
-    """
-    def test_isvalid_msid(self):
-        """
-        The test_isvalid_msid returns True if the submitted msid exists in the database.
-        Argument = msidn
-        """
-        creditairtime = creditAirtime()
-        self.assertEqual(creditairtime.msidnvalidity(22222),True)
-    def test_invalid_msidn(self):
-        """
-        The test_invalid_msidn returns False if the submitted msid doesn't exist in the database.
-        Argument = msidn
-        """
-        creditairtime = creditAirtime()
-        self.assertEqual(creditairtime.msidnvalidity(22222),False)
-    def test_isvalid_paying_account(self):
-        """
-        The test_isvalid_paying_account returns True if the submitted msid exists in the database.
-        Argument = msidn
-        """
-        creditairtime = creditAirtime()
-        self.assertEqual(creditairtime.paying_account(24422),True)
-    def test_invalid_paying_account(self):
-        """
-        The test_invalid_paying_account returns True if the submitted msid exists in the database.
-        Argument = msidn
-        """
-        creditairtime = creditAirtime()
-        self.assertEqual(creditairtime.paying_account(24422),False)
-    def test_balance_status(self):
-        """
-        The test_balance_status returns True if the submitted msid has balance greater than 0 on the wallet.
-        """
-        creditairtime = creditAirtime()
-        self.assertEqual(creditairtime.check_Crediting_Account_Balance(24422)>0,True)
-        self.assertEqual(creditairtime.check_Crediting_Account_Balance(24422)<=0,False)
-    def test_account_bal_GTE(self):
-        """
-        The test_isvalid_returns True if the submitted msid exists in the database.
-        Argument = msidn
-        """
-        creditairtime = creditAirtime()
-        balance = 500
-        self.assertEqual(creditairtime.check_isaccount_bal_GTE(24422,1000>=balance),True)
-    def test_account_bal_LTE(self):
-        """
-        The test_account_bal_LTE True is the submitted msid exists in the database.
-        Parm  = msidn
-        """
-        balance = 500
-        creditairtime = creditAirtime()
-        self.assertEqual(creditairtime.check_isaccount_bal_GTE(24422,300<=balance),True)
-    def test_deduct_amount(self):
-        creditairtime = creditAirtime()
-        
-        self.assertEqual(creditairtime.deduct_amount_from_msidn(10000),True)
-class testDebitAirtimeMethods(unittest.TestCase):
-    def test_msidn_isvalidity(self):
-        debitairtime = debitAirtime()
-        self.assertEqual(debitairtime.msidnvalidity(22222),True)
-    def test_msidn_invalidity(self):
-        debitairtime = debitAirtime()
-        self.assertEqual(debitairtime.msidnvalidity(22222),False)
-    #def test_msidn_balance_status(self):
-        #debitairtime = debitAirtime()
-        #self.assertTrue(debitairtime.msidn_balance_status(22222),True)
-        #self.assertFalse(debitairtime.msidnvalidity(444),False)
-    def test_debit_bal_GTE_Amount(self):
-        """
-        The test_isvalid_returns True if the submitted msid exists in the database.
-        Argument = msidn
-        """
-        debitairtime = debitAirtime()
-        balance = 500
-        self.assertEqual(debitairtime.debit_isaccount_bal_GTE(24422,1000>=balance),True)
-    def test_debit_bal_LTE_Amount(self):
-        """
-        The test_account_bal_LTE True is the submitted msid exists in the database.
-        Parm X = msidn
-        """
-        balance = 500
-        debitairtime = debitAirtime()
-        self.assertEqual(debitairtime.debit_isaccount_bal_LTE(24422,300<=balance),True)
+import pytest
 
+
+from app.handlers.profile import (
+    account_balance,
+    account_info,
+    profile_status)
+from app.handlers.airtime_handler import creditAirtime, debitAirtime
+
+from app.models.user import User
+
+
+def test_isvalid_msid():
+    credit = creditAirtime()
+    msisdn = '712306172'
+    current_user = User(mml_username='pkgmml', mml_password='pkgmml99')
+
+    # Act
+    result = credit.msidnvalidity(msisdn, current_user)
+
+    # Assert
+    assert isinstance(result, bool)
+
+
+def test_invalid_msidn():
+    credit = creditAirtime()
+    msisdn = '778306172'
+    current_user = User(mml_username='pkgmml', mml_password='pkgmml99')
+
+    # Act
+    result = credit.msidnvalidity(msisdn, current_user)
+
+    # Assert
+    assert isinstance(result, bool)
+
+
+def test_isvalid_paying_account():
+    credit = creditAirtime()
+    msisdn = '712306172'
+    current_user = User(mml_username='pkgmml', mml_password='pkgmml99')
+
+    # Act
+    result = credit.paying_account(msisdn, current_user)
+
+    # Assert
+    assert isinstance(result, bool)
+
+
+def test_invalid_paying_account():
+    credit = creditAirtime()
+    msisdn = '7783016172'
+    current_user = User(mml_username='pkgmml', mml_password='pkgmml99')
+
+    # Act
+    result = credit.paying_account(msisdn, current_user)
+
+    # Assert
+    assert isinstance(result, bool)
+
+
+def test_balance_status():
+    credit = creditAirtime()
+    msisdn = '712306172'
+    current_user = User(mml_username='pkgmml', mml_password='pkgmml99')
+
+    # Act
+    result = credit.check_Crediting_Account_Balance(msisdn, current_user)
+
+    # Assert
+    assert isinstance(result, str)
+
+
+def test_account_bal_GTE():
+    credit = creditAirtime()
+    msisdn = '712306172'
+    amount = 2000.0
+    current_user = User(mml_username='pkgmml', mml_password='pkgmml99')
+
+    # Act
+    result = credit.check_is_account_bal_GTE(msisdn, current_user, amount)
+
+    # Assert
+    assert isinstance(result, bool)
+
+
+def test_account_bal_LTE():
+    credit = creditAirtime()
+    msisdn = '712306172'
+    amount = 2000.0
+    current_user = User(mml_username='pkgmml', mml_password='pkgmml99')
+
+    # Act
+    result = credit.check_is_account_bal_GTE(msisdn, current_user, amount)
+
+    # Assert
+    assert isinstance(result, bool)
+
+
+def test_deduct_amount():
+    credit = creditAirtime()
+    msisdn = '712306172'
+    amount = 100.0
+    current_user = User(mml_username='pkgmml', mml_password='pkgmml99')
+
+    # Act
+    result = credit.deduct_amount_from_msidn(msisdn, current_user, amount)
+
+    # Assert
+    assert isinstance(result, str)
+
+def test_credit_amount():
+    credit = creditAirtime()
+    msisdn = '712306172'
+    amount = 100.0
+    current_user = User(mml_username='pkgmml', mml_password='pkgmml99')
+
+    # Act
+    result = credit.credit_msidn(msisdn, current_user, amount)
+
+    # Assert
+    assert isinstance(result, str)
+
+def test_msidn_isvalidity():
+    debitairtime = debitAirtime()
+    msisdn = '712306172'
+    current_user = User(mml_username='pkgmml', mml_password='pkgmml99')
+
+    # Act
+    result = debitairtime.msidnvalidity(msisdn, current_user)
+
+    # Assert
+    assert isinstance(result, bool)
+
+
+def test_msidn_invalidity():
+    debitairtime = debitAirtime()
+    msisdn = '778306172'
+    current_user = User(mml_username='pkgmml', mml_password='pkgmml99')
+
+    # Act
+    result = debitairtime.msidnvalidity(msisdn, current_user)
+
+    # Assert
+    assert isinstance(result, bool)
+
+
+def test_debit_bal_GTE_Amount():
+    debitairtime = debitAirtime()
+    msisdn = '712306172'
+    amount = 500.0
+    current_user = User(mml_username='pkgmml', mml_password='pkgmml99')
+
+    # Act
+    result = debitairtime.debit_is_account_bal_GTE(msisdn, current_user,amount)
+
+    # Assert
+    assert isinstance(result, bool)
+
+
+
+def test_debit_bal_LTE_Amount():
+    debitairtime = debitAirtime()
+    msisdn = '712306172'
+    amount = 500.0
+    current_user = User(mml_username='pkgmml', mml_password='pkgmml99')
+
+    # Act
+    result = debitairtime.debit_is_account_bal_LTE(msisdn, current_user,amount)
+
+    # Assert
+    assert isinstance(result, bool)
+
+
+def test_debit_deduct_amount():
+    debitairtime = debitAirtime()
+    msisdn = '712306172'
+    amount = 500.0
+    current_user = User(mml_username='pkgmml', mml_password='pkgmml99')
+
+    # Act
+    result = debitairtime.deduct_amount(msisdn, current_user,amount)
+
+    # Assert
+    assert isinstance(result, bool)
+
+    
