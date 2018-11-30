4271d89582e5c5cb0be8e27c711333071784fd37
Joseph Serunjogi <joeseggie@gmail.com>
2018-11-30 14:52:47 +0300

    Testing the response when user is does not have permission to create a user

--- tests/unit_tests/route_tests/test_accounts_routes.py
+++ tests/unit_tests/route_tests/test_accounts_routes.py
@@ -127,20 +127,23 @@ def test_create_account(
         assert response.status_code == 200
 
 
+@patch('app.models.user.User.query')
+@patch('jwt.decode')
 @patch.object(User, 'is_admin')
 @patch.object(User, 'is_active')
 @patch('app.db.session.commit')
 @patch('app.db.session.add')
 @patch('app.models.user.User.set_password')
 @patch('app.models.user.User.is_valid')
-@pytest.mark.skip(reason='Clean up')
 def test_create_account_invalid_user(
         mock_is_valid,
         mock_set_password,
         mock_add,
         mock_commit,
         mock_is_active,
-        mock_is_admin
+        mock_is_admin,
+        mock_token_decoder,
+        mock_user_query
 ):
     """Test authorized request returns success HTTP status code.
     """
@@ -150,6 +153,17 @@ def test_create_account_invalid_user(
     mock_commit = Mock()
     mock_is_valid.return_value = False
     mock_set_password.return_value = 'mock_password'
+    mock_user_query.filter_by.return_value.first.return_value = User(
+        username='vasuser',
+        public_id='f974c6bb-862d-4138-9fe4-b3ea1b70c7d2',
+        is_admin=False,
+        is_active=True
+    )
+    mock_token_decoder.return_value = {
+        'public_id': 'public_id=f974c6bb-862d-4138-9fe4-b3ea1b70c7d2',
+        'is_admin': False,
+        'is_active': True
+    }
     account_creator = {
         'Username': 'mock_user',
         'IsAdmin': True,
@@ -163,7 +177,11 @@ def test_create_account_invalid_user(
     with app.test_client() as app_test:
         headers = {
             'ContentType': 'application/json',
-            'dataType': 'json'
+            'dataType': 'json',
+            'x-access-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwdWJs'
+                              'aWNfaWQiOiI3ODM1MGFlMy0yYThiLTQzYmItYWVmMS02M'
+                              'WE3YWI1NGM4ODUiLCJleHAiOjE1NDI3OTY1NTF9.4HCZN'
+                              '00ppXyhg8KnkZ_mTABe-9q60Fw-bro3HlBUSR4'
         }
         response = app_test.post(
             '/inservices/api/v1.0/accounts/create',
@@ -172,5 +190,10 @@ def test_create_account_invalid_user(
             headers=headers,
             follow_redirects=True
         )
+        json_response_data = response.get_json()
 
-        assert response.status_code == 409
+        assert response.status_code == 403
+        assert json_response_data['operationResult'] == 'FORBIDDEN: You do ' \
+                                                        'not have the' \
+                                                        ' permission to' \
+                                                        ' perform operation.'
